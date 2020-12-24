c11da0ce3239706da6a69aadde80d5a430495aec
Cristian Blanco <cb@qbkconsulting.com>
2020-12-24 11:50:31 -0300

    books component

--- openlibraryfront/src/app/app.module.ts
+++ openlibraryfront/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BooksComponent } from './components/books/books.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
---
+++ openlibraryfront/src/app/models/Book.ts
@@ -0,0 +1,7 @@
+export class Book {
+  id:number;
+  title:string;
+  isbn10:string;
+  isbn13:string;
+  description:string;
+}
