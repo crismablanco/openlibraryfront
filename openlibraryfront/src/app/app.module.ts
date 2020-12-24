3d66b61c70946e62e5cd23a9195a7bd69ed66bf9
Cristian Blanco <cb@qbkconsulting.com>
2020-12-24 11:53:30 -0300

    Add book item component

--- openlibraryfront/src/app/app.module.ts
+++ openlibraryfront/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BooksComponent } from './components/books/books.component';
-import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { BookItemComponent } from './components/book-item/book-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksComponent,
-    TodoItemComponent
+    BookItemComponent,
   ],
   imports: [
     BrowserModule
